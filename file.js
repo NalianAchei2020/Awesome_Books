aa9b190495ca9e2f58a088bd269ae570f53613ab
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-07-05 16:23:22 +0100

    add navbar

--- file.js
+++ file.js
@@ -31,6 +31,7 @@ class BookCollection {
     }
   }
 
+  // display books
   displayBooks() {
     const bookList = document.getElementById('bookTable');
     bookList.innerHTML = '';
@@ -72,4 +73,47 @@ function addBook() {
 }
 
 document.getElementById('add').addEventListener('click', addBook);
-window.addEventListener('load', () => bookCollection.displayBooks());
+
+// A Navigation bar.
+function showSection(id) {
+  const sections = document.querySelectorAll('main section');
+  sections.forEach((section) => {
+    if (section.id === id) {
+      section.classList.remove('hidden');
+    } else {
+      section.classList.add('hidden');
+    }
+  });
+}
+
+document.getElementById('books-link').addEventListener('click', () => {
+  showSection('books-section');
+});
+
+document.getElementById('add-book-link').addEventListener('click', () => {
+  showSection('add-book-section');
+});
+
+document.getElementById('contact-link').addEventListener('click', () => {
+  showSection('contact-section');
+});
+
+window.addEventListener('load', () => {
+  bookCollection.displayBooks();
+  showSection('books-section');
+});
+
+// active link
+const link = document.querySelectorAll('.link');
+function removeActive() {
+  link.forEach((link) => {
+    link.classList.remove('active');
+  });
+}
+
+link.forEach((link) => {
+  link.addEventListener('click', () => {
+    removeActive();
+    link.classList.add('active');
+  });
+});
