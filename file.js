aeb0bf01625e1d32f318dfe068060c1501ff26b5
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-07-05 10:14:10 +0100

    add file

--- file.js
+++ file.js
@@ -1,44 +1,40 @@
 /* eslint-disable no-use-before-define */
-const bookTitle = document.getElementById('bookTitle');
-const bookAuthor = document.getElementById('bookauthor');
-const addButton = document.getElementById('add');
-const main = document.querySelector('.main');
+const books = [];
 
-let books = JSON.parse(localStorage.getItem('books')) || [];
-
-const addBook = (title, author) => {
-  const book = { title, author };
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  display();
-};
-const removeBook = (title) => {
-  books = books.filter((book) => book.title !== title);
-  localStorage.setItem('books', JSON.stringify(books));
-  display();
-};
-const display = () => {
-  main.innerHTML = '';
+function displayBooks() {
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
   books.forEach((book) => {
-    const span = document.createElement('span');
-    span.textContent = `${book.title} by ${book.author}`;
-    const br = document.createElement('br');
-    span.appendChild(br);
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => removeBook(book.title));
-    span.appendChild(removeButton);
-    const line = document.createElement('hr');
-    span.appendChild(line);
-    main.appendChild(span);
+    const li = document.createElement('li');
+    const text = document.createTextNode(`${book.title} by ${book.author}`);
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remove';
+    removeBtn.addEventListener('click', () => removeBook(book));
+    li.appendChild(text);
+    li.appendChild(removeBtn);
+    bookList.appendChild(li);
   });
-};
-addButton.addEventListener('click', () => {
-  const title = bookTitle.value;
-  const author = bookAuthor.value;
-  addBook(title, author);
-  bookTitle.value = '';
-  bookAuthor.value = '';
-});
+}
 
-display();
+function addBook() {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const book = {
+    title,
+    author,
+  };
+  books.push(book);
+  displayBooks();
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+}
+
+function removeBook(book) {
+  const index = books.indexOf(book);
+  if (index > -1) {
+    books.splice(index, 1);
+  }
+  displayBooks();
+}
+
+document.getElementById('add-btn').addEventListener('click', addBook);
